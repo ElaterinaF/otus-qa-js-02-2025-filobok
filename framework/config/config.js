42aa33b6a3242776f9ce9cc3fde0380edf0bbd55
ElaterinaF <ekaterina.filobok@yandex.ru>
2025-04-09 21:43:04 +0300

    HW

---
+++ framework/config/config.js
@@ -0,0 +1,7 @@
+export const credentials = {
+    login: process.env.TEST_BOOKSTORE_LOGIN,
+    password: process.env.TEST_BOOKSTORE_PASSWORD,
+    userId: process.env.TEST_BOOKSTORE_USER_ID
+  }
+  
+  export const baseURL = process.env.TEST_BOOKSTORE_URL
